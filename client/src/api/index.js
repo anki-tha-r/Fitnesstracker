3275c0ce232b338db10d68d59f281742272d8e92
Ankitha R <130216104+anki-tha-r@users.noreply.github.com>
2024-12-19 00:28:12 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://fitnesstracker-3-4hat.onrender.com/api/",
+  baseURL: "https://fitnesstracker-4-yx8y.onrender.com/api/",
 });
 
 export const UserSignUp = async (data) => API.post("/user/signup", data);
